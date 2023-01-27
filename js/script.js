61b06a99c277b7a7ac2513690f1ffa4d7e72c105
ochosteve <ochosteve08@gmail.com>
2023-01-27 08:42:03 +0100

    fixed design bugs, modified hamburger and mobile-menu

--- js/script.js
+++ js/script.js
@@ -5,58 +5,31 @@ $(window).on("load", function () {
   }, 3000);
 });
 
-const toggleMenuBtn = document.querySelector("#toggle-menu");
-const closeMenuBtn = document.querySelector("#close-btn");
+
 const mobileMenu = document.querySelector(".mobile-menu");
 const overlay = document.querySelector(".overlay");
-const toggleDropdownMenu = document.querySelectorAll(".menu-item .item");
-
-console.log(toggleDropdownMenu);
-toggleMenuBtn.addEventListener("click", () => {
-  mobileMenu.classList.add("active");
-  overlay.classList.add("active");
-});
-
-closeMenuBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("active");
-  overlay.classList.remove("active");
-});
+const btn = document.getElementById('menu-btn');
 
 
-// $(window).ready(function () {
-//   let mouseX = 0;
-//   let mouseY = 0;
-//   let xp = 0;
-//   let yp = 0;
 
-//   $(document).mousemove(function (e) {
-//     $(".custom__cursor__inner").css({
-//       transform:
-//         "translate(" + (e.clientX - 3.25) + "px, " + (e.clientY - 3.25) + "px)",
-//     });
+let dropdown =()=>{
 
-//     mouseX = e.clientX - 10;
-//     mouseY = e.clientY - 10;
-//   });
+    mobileMenu.classList.toggle("active");
+    btn.classList.toggle('open');
+    overlay.classList.toggle("active");
+   
+}
 
-//   setInterval(function () {
-//     xp += (mouseX - xp) / 6;
-//     yp += (mouseY - yp) / 6;
-//     $(".custom__cursor__outer").css({
-//       transform:
-//         "translateX(" + (xp - 15) + "px) translateY(" + (yp - 15) + "px)",
-//     });
-//   }, 10);
-// });
-
-
+//hamburger menu dropdown
+btn.addEventListener('click',(event)=>{
+    event.stopPropagation();
+    dropdown();
+})
 
 
 // hamburger menu closes on click outside of the menu
 document.documentElement.addEventListener("click", function () {
-  if  (mobileMenu.classList.contain("active")) {
-    console.log("Mobile")
-    mobileMenu.classList.remove("active");
-  overlay.classList.remove("active");
+  if (btn.classList.contains("open")) {
+    dropdown();
   }
 });
